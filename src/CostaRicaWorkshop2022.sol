4e376249d43919b0827f86b622b6aadd6cfa258a
Igor Yalovoy <to@ylv.io>
2022-12-02 17:19:59 -0600

    Add mint function

--- src/CostaRicaWorkshop2022.sol
+++ src/CostaRicaWorkshop2022.sol
@@ -5,21 +5,56 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 
+/*
+ ______     ______     ______     ______   ______        ______     __     ______     ______    
+/\  ___\   /\  __ \   /\  ___\   /\__  _\ /\  __ \      /\  == \   /\ \   /\  ___\   /\  __ \   
+\ \ \____  \ \ \/\ \  \ \___  \  \/_/\ \/ \ \  __ \     \ \  __<   \ \ \  \ \ \____  \ \  __ \  
+ \ \_____\  \ \_____\  \/\_____\    \ \_\  \ \_\ \_\     \ \_\ \_\  \ \_\  \ \_____\  \ \_\ \_\ 
+  \/_____/   \/_____/   \/_____/     \/_/   \/_/\/_/      \/_/ /_/   \/_/   \/_____/   \/_/\/_/ 
+ */
+
 /// @custom:security-contact to@ylv.io
 contract CostaRicaWorkshop2022 is ERC721, Ownable {
     using Counters for Counters.Counter;
 
+    /* ============ Storage ============ */
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721('CostaRicaWorkshop2022', 'CRW') {}
+    /* ============ Constants ============ */
 
-    function _baseURI() internal pure override returns (string memory) {
-        return 'ipfs://bafybeidlkqhddsjrdue7y3dy27pu5d7ydyemcls4z24szlyik3we7vqvam/';
+    /* ============ Immutables ============ */
+    uint256 public immutable eventEndsTS;
+    uint256 public immutable eventStartsTS;
+
+    /* ============ Events ============ */
+
+    /* ============ Constructor ============ */
+
+    constructor() ERC721('CostaRicaWorkshop2022', 'CRW') {
+        eventStartsTS = block.timestamp;
+        eventEndsTS = block.timestamp + 1 days;
     }
 
-    function safeMint(address to) public onlyOwner {
+    /* ============ Modifiers ============ */
+    modifier isEventOpen() {
+        require(block.timestamp < eventEndsTS, 'Event is not open');
+        _;
+    }
+
+    modifier isEventOver() {
+        require(block.timestamp > eventEndsTS, 'Event is not over');
+        _;
+    }
+
+    /* ============ External Write Functions ============ */
+
+    function mint(address to) public isEventOpen {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
+
+    /* ============ External View Functions ============ */
+
+    /* ============ Internal View Functions ============ */
 }
--- test/CostaRicaWorkshop2022.t.sol
+++ test/CostaRicaWorkshop2022.t.sol
@@ -7,7 +7,29 @@ import {CostaRicaWorkshop2022} from '../src/CostaRicaWorkshop2022.sol';
 contract CounterTest is Test {
     CostaRicaWorkshop2022 public workshop;
 
+    address payable internal alice = payable(makeAddr('alice'));
+    address payable internal bob = payable(makeAddr('bob'));
+
     function setUp() public {
+        vm.label(alice, 'alice');
+        vm.label(bob, 'bob');
+
         workshop = new CostaRicaWorkshop2022();
     }
+
+    /* ============ External Write Functions ============ */
+
+    // write a test for mint function
+    function testMint() public {
+        // prank msg.sender to be alice
+        vm.prank(alice);
+
+        workshop.mint(alice);
+
+        assertEq(workshop.balanceOf(alice), 1);
+        // check that erc721 token id is equal to 0 using ownerOf method
+        assertEq(workshop.ownerOf(0), alice);
+    }
+
+    /* ============ External View Functions ============ */
 }
